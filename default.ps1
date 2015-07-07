dd0958beda3704733f4029e8702871f0eb0d4665
Joe Young <joe@madmoosesoftware.com>
2015-07-07 11:25:21 -0500

    implemented build script

--- default.ps1
+++ default.ps1
@@ -14,11 +14,20 @@ properties {
     $nugetDirectory = Join-Path -path $sourceDirectory -childPath ".nuget"
     $packageDirectory = Join-Path -path $baseDirectory -childPath "NuGet"
     $nuget = Join-Path -path $nugetDirectory -childPath "nuget.exe"
+	$nuspec = Join-Path -path $sourceDirectory -childPath (Join-Path -path "MadMoose.CQRS" -childPath "MadMoose.CQRS.nuspec")
+}
+
+#nunit properties 
+properties {
+    $nunitDirectory = Join-Path -path $sourceDirectory -childPath "packages\NUnit.Runners.2.6.4\tools"
+    $nunit = Join-Path -path $nunitDirectory -childPath "nunit-console.exe"
+	$reportDirectory = Join-Path -path $baseDirectory -childPath "Reports"
+    $nunitReportFile = Join-Path -path $reportDirectory -childPath "TestResults.xml"
 }
 
 #project properties
 properties {
-    $solution = Join-Path -path $sourceDirectory -childPath "MySolution.sln"
+    $solution = Join-Path -path $sourceDirectory -childPath "MadMoose.CQRS.sln"
 }
 
 #import the default build tasks
@@ -28,6 +37,39 @@ properties {
 
 task Do-It {
     Write-Output $baseDirectory
+}
+
+task Run-Specs -Description "Runs all specifications" -Depends Build-Debug {
+
+	if (-not(Test-Path $reportDirectory -PathType Container)) 
+	{
+		New-Item -ItemType Directory -Path $reportDirectory
+	}
+
+    $path = Join-Path -path $sourceDirectory -childPath ("**\bin\Debug\MadMoose.CQRS.Specifications.dll")
+    
+    $assemblies = Get-ChildItem $path
+
+    if ($assemblies.count -gt 0 ) {
+        # need to use the call operator "&" to call out to nunit, this is unlike msbuild which is built into psake
+        exec { & $nunit $assemblies /xml=$nunitReportFile /framework:net-4.5 /nologo}
+    } else {
+        Write-Output "No assemblies found to execute tests"
+    }
+}
+
+# The pack (and release) tasks require a version parameter, you can pass the version on the command line
+# like  invoke-psake pack-release -parameters @{"version"="1.2.3.4"}
+task Pack-Release -Description "Creates a NuGet package based on the release build" -depends Build-Init {
+           
+	$version = Get-VersionFn $nuspec $versionOverride
+    
+	Write-Host "Release Version: $version"
+	
+    Build-ReleaseFn $version
+   
+    #create the package
+    exec {nuget pack $nuspec -Version $version -OutputDirectory $packageDirectory}
 }
 
 #-------------------------------------------------------------------------------
