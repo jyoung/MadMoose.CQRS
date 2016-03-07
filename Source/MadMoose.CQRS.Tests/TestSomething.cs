db66781ef1ca91a2ca6c6d848e553214d1561d9f
Joe Young <joe@madmoosesoftware.com>
2016-03-06 21:44:25 -0600

    added new test project

---
+++ Source/MadMoose.CQRS.Tests/TestSomething.cs
@@ -0,0 +1,14 @@
+﻿namespace MadMoose.CQRS.Tests
+{
+    using Shouldly;
+    using Xunit;
+
+    public class TestSomething
+    {
+        [Fact]
+        public void it_should_do_something()
+        {
+            true.ShouldBeTrue();
+        }
+    }
+}
